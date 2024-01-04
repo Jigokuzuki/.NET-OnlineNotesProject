f5cb6fa841cbf74f5c7fd02029a922111efe7786
Jigokuzuki <maciek.gorski888@wp.pl>
2024-01-04 18:31:27 +0100

    Mapping Notes Endpoints + MinimalApis.Extensions

--- OnlineNotes.Api/Entities/Note.cs
+++ OnlineNotes.Api/Entities/Note.cs
@@ -1,11 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace OnlineNotes.Api.Entities;
 
 public class Note
 {
     public int Id { get; set; }
+
+    [Required]
+    [StringLength(50)]
     public required string Title { get; set; }
+
+    [Required]
     public required string Content { get; set; }
     public DateTimeOffset CreatedDate { get; set; }
     public DateTimeOffset ModifiedDate { get; set; }
+
+    [Required]
+    [StringLength(20)]
     public required string Category { get; set; }
 }
--- OnlineNotes.Api/obj/Debug/net7.0/OnlineNotes.Api.AssemblyInfo.cs
+++ OnlineNotes.Api/obj/Debug/net7.0/OnlineNotes.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineNotes.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48e529656bf3715da5e9327716d9c461e7e58e01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+162d840b405a04d499657e298560f1e0b5789b68")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineNotes.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineNotes.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
